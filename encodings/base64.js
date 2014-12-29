7c274cd165806a3908fba8befc27a2f28e862873
vivek poddar <vivekimsit@gmail.com>
2014-12-29 16:05:22 +0530

    Create base64.js

---
+++ encodings/base64.js
@@ -0,0 +1,16 @@
+/**
+ * Base 64 encoding in javascript. Its basically binary-to-text encoding scheme.
+ * 
+ */
+
+/**
+ * @param {string} text The text to encode.
+ * @return {string} The base64 encoded string.
+ */
+function base64Encode() {}
+
+/**
+ * @param {string} text The text to decode.
+ * @return {string} The base64 decoded string.
+ */
+function base64Decode() {}
